e0c73cc9ab3258f84d0a71b6ceb39076ebfccc76
liuweihust <liuweiapply@hotmail.com>
2018-07-26 17:10:14 +0800

    seller post ok

--- client/postdata.go
+++ client/postdata.go
@@ -14,6 +14,8 @@ import (
 	"strconv"
 )
 
+var verbose *bool
+
 type CipherMessage struct {
 	Type   string `json:"cipher"`
 	Cipher string `json:"data"`
@@ -40,13 +42,13 @@ func Load(filename string) (msg interface{}, err error) {
 	err = nil
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		fmt.Printf("error:%v\n", err)
+		fmt.Printf("Error: read file%v\n", err)
 		return
 	}
 
 	err = json.Unmarshal([]byte(data), &msg)
 	if err != nil {
-		fmt.Printf("Parse json error:%v\n", err)
+		fmt.Printf("Error: Unmarshal json:%v\n", err)
 		return
 	}
 	return
@@ -55,7 +57,7 @@ func Load(filename string) (msg interface{}, err error) {
 func signdata(data interface{}) (*string, error) {
 	msg, err := json.Marshal(data)
 	if err != nil {
-		fmt.Printf("Marshal error:%v\n", err)
+		fmt.Printf("Error:Marshal json error:%v\n", err)
 		panic(2)
 	}
 
@@ -67,8 +69,11 @@ func signdata(data interface{}) (*string, error) {
 
 	urldata, err := json.Marshal(*sigdata)
 	if err != nil {
-		fmt.Printf("marshal Err:%v\n", sigdata)
+		fmt.Printf("Error:Marshal json:%v\n", sigdata)
 		return nil, err
+	}
+	if *verbose {
+		fmt.Printf("marshal res:%s\n", urldata)
 	}
 	newdata := base64.URLEncoding.EncodeToString(urldata)
 
@@ -89,7 +94,7 @@ func builddata(sigmsg *string) (postdata *PostMessage, err error) {
 	return pdata, nil
 }
 
-func postdata(data *PostMessage, server string, port int) error {
+func postdata(data *PostMessage, server string, port int) ([]byte, error) {
 	body := new(bytes.Buffer)
 	json.NewEncoder(body).Encode(*data)
 
@@ -97,16 +102,15 @@ func postdata(data *PostMessage, server string, port int) error {
 
 	rsp, err := http.Post(newurl, "content-type:text/plain;", body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer rsp.Body.Close()
 	body_byte, err := ioutil.ReadAll(rsp.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	fmt.Println(string(body_byte))
 
-	return nil
+	return body_byte, nil
 }
 
 func main() {
@@ -114,16 +118,19 @@ func main() {
 	key := flag.String("key", "prvkey.json", "private key file to read")
 	server := flag.String("server", "127.0.0.1", "server address")
 	port := flag.Int("port", 26657, "server port")
-	cipher := flag.String("cipher", "pubkey", "whether to encipher data,options:plain,symm,pubkey")
+	cipher := flag.String("cipher", "plain", "whether to encipher data,options:plain,symm,pubenc")
 	pubkey := flag.String("pubkey", "", "receiver's pubkey")
-	mode := flag.String("mode", "postdata", "Task:postdata,pub(show pubkey)")
+	mode := flag.String("mode", "postdata", "Task:postdata,pub(show pubkey),pubenc(using pubkey to encode data)")
+	verbose = flag.Bool("v", false, "Verbose:default false")
+	plain := flag.String("plain", "", "data to encode using pubkey")
 
 	flag.Parse()
+
 	var err error
 
 	err = lib.Generate(*key)
 	if err != nil {
-		fmt.Printf("GetKey error:%v\n", err)
+		fmt.Printf("Error:GetKey:%v\n", err)
 		panic(2)
 	}
 
@@ -131,22 +138,44 @@ func main() {
 	case "pub":
 		fmt.Print(hex.EncodeToString(lib.Getpubkey()))
 		return
+	case "pubenc":
+		if *plain == "" {
+
+			fmt.Println("Error:Must provide data to be encoded!")
+			return
+		}
+		if *pubkey == "" {
+			fmt.Println("Error:Must provide pubkey to encod data!")
+			return
+		}
+		lib.ImportReceiver(*pubkey)
+		cipher, err := lib.SendReceiver([]byte(*plain))
+		if err != nil {
+			fmt.Printf("Error:Encode with pubkey:%v\n", err)
+			return
+		}
+		fmt.Printf("%s", hex.EncodeToString(cipher))
+		return
+	case "postdata":
 	default:
+		fmt.Printf("Error:Mode:%s\n", *mode)
+		return
 	}
 
 	data, err := Load(*file)
 	dataf, ok := data.(map[string]interface{})
 	if !ok {
-		fmt.Printf("Load file error:%v\n", err)
+		fmt.Printf("Error:Load file:%v\n", err)
 		return
 	}
-	//fmt.Printf("data[data]=%v\n", dataf["data"])
-
+	if *verbose {
+		fmt.Printf("data[data]=%v\n", dataf["data"])
+	}
 	switch *cipher {
 	case "symm":
 		msg, err := json.Marshal(dataf["data"])
 		if err != nil {
-			fmt.Printf("Marshal error:%v\n", err)
+			fmt.Printf("Error:Marshal json:%v\n", err)
 			panic(2)
 		}
 
@@ -172,20 +201,20 @@ func main() {
 		*/
 	case "pubkey":
 		if (*pubkey) == "" {
-			fmt.Println("Using pubkey enciper must provide pubkey")
+			fmt.Println("Error:Using pubkey enciper must provide pubkey")
 			return
 		}
 		lib.ImportReceiver(*pubkey)
 
 		msg, err := json.Marshal(dataf["data"])
 		if err != nil {
-			fmt.Printf("Marshal error:%v\n", err)
+			fmt.Printf("Error:Marshal json:%v\n", err)
 			panic(2)
 		}
 
 		cipher, err := lib.SendReceiver(msg)
 		if err != nil {
-			fmt.Printf("Marshal error:%v\n", err)
+			fmt.Printf("Error:encode with pubkey:%v\n", err)
 			panic(2)
 		}
 		dataf["encode"] = "pubkey"
@@ -194,7 +223,7 @@ func main() {
 			//Blow code will decode the cipher encoded by pubkey
 			decipher, err := lib.PrvDecrypt(cipher)
 			if err != nil {
-				fmt.Printf("Prv Decipher error:%v\n", err)
+				fmt.Printf("Error:Prv Decipher:%v\n", err)
 				panic(2)
 			}
 			fmt.Printf("PrvDecrypt:%s\n", decipher)
@@ -202,27 +231,33 @@ func main() {
 		*/
 	case "plain":
 	default: //switch *cipher
-		fmt.Println("Cipher mode Error")
+		fmt.Println("Error:Cipher mode")
 		return
 	}
 
 	sdata, err := signdata(dataf)
 	if err != nil {
-		fmt.Printf("Sign data error:%v\n", err)
+		fmt.Printf("Error:Sign data:%v\n", err)
 		return
 	}
-	//fmt.Printf("signdata=%v\n", *sdata)
+	if *verbose {
+		fmt.Printf("signdata=%v\n", *sdata)
+	}
 
 	pdata, err := builddata(sdata)
 	if err != nil {
-		fmt.Printf("Build data error:%v\n", err)
+		fmt.Printf("Error:Build data:%v\n", err)
 		return
 	}
-	fmt.Printf("pdata=%+v\n", pdata)
-
-	err = postdata(pdata, *server, *port)
+	if *verbose {
+		fmt.Printf("pdata=%+v\n", pdata)
+	}
+	resp, err := postdata(pdata, *server, *port)
 	if err != nil {
-		fmt.Printf("Marshal error:%v\n", err)
+		fmt.Printf("Error:post data:%v\n", err)
 		return
+	}
+	if *verbose {
+		fmt.Println(string(resp))
 	}
 }
---
+++ examples/run_seller_trans.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env 
+import os
+import json  
+import tempfile
+import re
+
+FilePath="./"
+DataFiles=["data_general.json","data_bloodnormal.json","data_smoke.json"]
+DataAll="data_all.json"
+TransData="trans.json"
+BinFile="postdata"
+BuyerPrvFile="buyer_prvkey.json"
+SellerPrvFile="seller_prvkey.json"
+
+cmd=BinFile + " -mode pub -key " + SellerPrvFile
+seller=os.popen(cmd).read()
+seller_utf8 = u'%s'%seller
+
+detepat = re.compile('^Secret:.*$')
+pubenc_keys=[]
+for item in DataFiles:
+    cmd=BinFile + " -file " + FilePath+item + " -cipher symm"
+    o=os.popen(cmd).read()
+    res = re.search(r'^Secret:.*',o) 
+    find = res.group()
+    if (len(find)) == 0:
+        print("Can't find secret:%s"%o)
+        break
+    split = find.split(":", 1)
+    cmd=BinFile + " -mode pubenc -pubkey \"" + seller_utf8 + "\" -plain " + split[1]
+    print("cmd:%s\n"%(cmd))
+    o=os.popen(cmd).read()
+    keyenc_utf8 = u'%s'%o
+    pubenc_keys.append(keyenc_utf8)
+
+print("enckeys=",pubenc_keys)
+
+with open(FilePath+TransData, 'r') as f:  
+    data = json.loads(f.read())  
+    for i in range(len(pubenc_keys)):
+        data['keys'][i]['DID']=pubenc_keys[i]
+    f.close()
+
+
+tmpfile = tempfile.mkstemp()
+print("temp file:%s"%(tmpfile[1]))
+jsonstr=json.dumps(data)
+with open(tmpfile[1], 'w') as f:  
+    f.write(jsonstr)
+    f.close()
+
+cmd=BinFile + " -file " + tmpfile[1] + " -cipher plain"
+o=os.popen(cmd).read()
+print(o)
